2d931578183ddde5a510d75bc2d7ef1cab4a3723
Rusmir Alexandru <rusmiralexandru@gmail.com>
2024-06-03 08:33:03 +0300

    2486.1

--- TwoPointers/2486appendCharToMakeStrSubsequence.ts
+++ TwoPointers/2486appendCharToMakeStrSubsequence.ts
@@ -2,7 +2,7 @@ const appendCharacters = (s: string, t: string): number => {
   let sPos = 0,
     tPos = 0;
 
-  while (sPos < s.length && tPos <= t.length) {
+  while (sPos < s.length && tPos < t.length) {
     if (s[sPos] === t[tPos]) {
       tPos++;
     }
